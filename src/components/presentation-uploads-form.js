c67329918cf69e4a5f0c281fbf75aef88f66ab9b
Santi <santipalenque@gmail.com>
2022-09-27 10:29:38 -0300

    add max limit and min mandatory (#24)
    * add max limit and min mandatory
    
    * adding max allowed label

--- src/components/presentation-uploads-form.js
+++ src/components/presentation-uploads-form.js
@@ -94,11 +94,11 @@ class PresentationUploadsForm extends React.Component {
 
         if(cur_event_type) {
             cur_event_type.allowed_media_upload_types.forEach(mediaUploadType => {
-                if(mediaUploadType.is_mandatory){
+                if(mediaUploadType.is_mandatory || mediaUploadType.min_uploads_qty > 0){
                     // check if user provided file
                     var mediaUploads = this.getMediaUploadsByType(entity, mediaUploadType);
-                    if(mediaUploads.length === 0){
-                        errors[mediaUploadType.name] = 'This field is required.';
+                    if(mediaUploads.length < mediaUploadType.min_uploads_qty){
+                        errors[mediaUploadType.name] = `This field is required. You must upload a minimum of ${mediaUploadType.min_uploads_qty} files`;
                     }
                 }
             });
@@ -140,13 +140,15 @@ class PresentationUploadsForm extends React.Component {
                     const notLastItem = i < allowedMediaUploads.length -1;
                     const allowedExt = media_type.type.allowed_extensions.map((ext) => `.${ext.toLowerCase()}`).join(",");
                     const mediaUploads = this.getMediaUploadsByType(entity, media_type);
+                    const maxFiles = media_type.max_uploads_qty > 0 ? media_type.max_uploads_qty : null;
 
                     return (
                         <div key={media_type.id} className={`row form-group ${notLastItem ? 'border' : ''}`}>
                             <div className="col-md-12">
                                 <label>
                                     {media_type.name} ({allowedExt}) - Max. Size {media_type.max_size/1024} MB
-                                    {media_type.is_mandatory && <i> - mandatory</i>}
+                                    {media_type.is_mandatory && <i> - {media_type.min_uploads_qty} mandatory</i>}
+                                    {maxFiles && <i> - {maxFiles} max. allowed</i>}
                                 </label>
                                 {
                                     media_type.description !== '' &&
@@ -161,6 +163,7 @@ class PresentationUploadsForm extends React.Component {
                                     postUrl={`${window.API_BASE_URL}/api/public/v1/files/upload`}
                                     error={this.hasErrors(media_type.name)}
                                     djsConfig={{withCredentials:true}}
+                                    maxFiles={maxFiles}
                                 />
                             </div>
                         </div>
